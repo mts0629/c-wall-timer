24a272aa0ebb7e8a585d61184d3391dabdc725be
mts0629 <soratobi93@gmail.com>
2023-10-14 00:49:35 +0900

    Add doxygen comments

--- src/cwt.c
+++ src/cwt.c
@@ -1,5 +1,15 @@
+/**
+ * @file cwt.c
+ * @brief Wall timer
+*/
 #include "cwt.h"
 
+/**
+ * @brief Calculate difference between two time points
+ * @param[in] start Start time
+ * @param[in] end End time
+ * @return Difference time between the timepoints
+*/
 static inline struct timespec get_diff_timespec(const struct timespec* start, const struct timespec* end) {
     time_t sec = end->tv_sec - start->tv_sec;
     long nsec = end->tv_nsec - start->tv_nsec;
@@ -18,7 +28,13 @@ void cwt_stop(cwt_timer* timer) {
     timespec_get(&timer->end, TIME_UTC);
 }
 
-static inline double convert_scale(const struct timespec* time, const cwt_scale scale) {
+/**
+ * @brief Convert time by specified timescale
+ * @param[in] time Time
+ * @param[in] scale Timescale
+ * @return Time expression in specified timescale
+*/
+static inline double convert_by_timescale(const struct timespec* time, const cwt_scale scale) {
     if (scale == CWT_SECONDS) {
         return time->tv_sec + time->tv_nsec / 1e9;
     } else if (scale == CWT_MILLISECONDS) {
@@ -32,21 +48,21 @@ static inline double convert_scale(const struct timespec* time, const cwt_scale 
 
 double cwt_get_time(const cwt_timer* timer, const cwt_scale scale) {
     struct timespec elapsed = get_diff_timespec(&timer->start, &timer->end);
-    return convert_scale(&elapsed, scale);
+    return convert_by_timescale(&elapsed, scale);
 }
 
 void cwt_lap_start(cwt_lap_timer* timer) {
     timer->lap_count = 0;
-    timespec_get(&timer->lap[timer->lap_count], TIME_UTC);
+    timespec_get(&timer->laps[timer->lap_count], TIME_UTC);
     timer->lap_count++;
 }
 
 void cwt_lap_record(cwt_lap_timer* timer) {
-    timespec_get(&timer->lap[timer->lap_count], TIME_UTC);
+    timespec_get(&timer->laps[timer->lap_count], TIME_UTC);
     timer->lap_count++;
 }
 
 double cwt_get_lap_time(cwt_lap_timer* timer, const int lap_index, const cwt_scale scale) {
-    struct timespec elapsed = get_diff_timespec(&timer->lap[lap_index], &timer->lap[lap_index + 1]);
-    return convert_scale(&elapsed, scale);
+    struct timespec elapsed = get_diff_timespec(&timer->laps[lap_index], &timer->laps[lap_index + 1]);
+    return convert_by_timescale(&elapsed, scale);
 }
--- src/cwt.h
+++ src/cwt.h
@@ -1,50 +1,100 @@
+/**
+ * @file cwt.h
+ * @brief Wall timer
+*/
 #ifndef CWT_H
 #define CWT_H
 
 #include <time.h>
 
+/**
+ * @brief Timer object
+*/
 typedef struct cwt_timer {
-    struct timespec start;
-    struct timespec end;
+    struct timespec start; //!< Start time
+    struct timespec end; //!< End time
 } cwt_timer;
 
+/**
+ * @brief Lap timer object
+*/
 typedef struct cwt_lap_timer {
-    int lap_count;
-    struct timespec* lap;
+    int lap_count; //!< Current lap count
+    struct timespec* laps; //!< Lap times
 } cwt_lap_timer;
 
+/**
+ * @brief Initialize a lap timer with specified max lap count
+*/
 #define CWT_LAP_TIMER_INIT(max_lap_count) { \
     .lap_count = 0, \
-    .lap = (struct timespec[(max_lap_count) + 1]){ \
+    .laps = (struct timespec[(max_lap_count) + 1]){ \
         { 0, 0 }, \
     } \
 }
 
+/**
+ * @brief Timescale for the elapsed time
+*/
 typedef enum cwt_scale {
-    CWT_SECONDS,
-    CWT_MILLISECONDS,
-    CWT_MICROSECONDS,
-    CWT_NANOSECONDS
+    CWT_SECONDS, //!< Seconds
+    CWT_MILLISECONDS, //!< Milliseconds
+    CWT_MICROSECONDS, //!< Microseconds
+    CWT_NANOSECONDS //!< Nanoseconds
 } cwt_scale;
 
+/**
+ * @brief Start the timer
+ * @param[out] timer Timer object
+*/
 void cwt_start(cwt_timer* timer);
 
+/**
+ * @brief Stop the timer
+ * @param[out] timer Timer object
+*/
 void cwt_stop(cwt_timer* timer);
 
+/**
+ * @brief Get elapsed time
+ * @param[in] timer Timer object
+ * @param[in] scale Timescale
+ * @return Elapsed time in specified timescale
+*/
 double cwt_get_time(const cwt_timer* timer, const cwt_scale scale);
 
-#define CWT_TIMER_BLOCK(time, scale, proc){ \
+/**
+ * @brief Get elapsed time for processing of specified codes
+ * @note Multi-line codes must be surrounded by curly blace `{}`
+*/
+#define CWT_TIMER_BLOCK(time, scale, codes){ \
     cwt_timer t_; \
     cwt_start(&t_); \
-    proc; \
+    codes; \
     cwt_stop(&t_); \
     (time) = cwt_get_time(&t_, (scale)); \
 }
 
+/**
+ * @brief Start the lap timer
+ * @param[out] timer Lap timer object
+*/
 void cwt_lap_start(cwt_lap_timer* timer);
 
+/**
+ * @brief Record the current lap time
+ * @param[out] timer Lap timer object
+ * @note The number of recording be within the max lap count of the timer object
+*/
 void cwt_lap_record(cwt_lap_timer* timer);
 
+/**
+ * @brief Get elapsed time of the specified lap
+ * @param[in] timer Timer object
+ * @param[in] lap_index Lap index
+ * @param[in] scale Timescale
+ * @return Elapsed time of the lap in specified timescale
+*/
 double cwt_get_lap_time(cwt_lap_timer* timer, const int lap_index, const cwt_scale scale);
 
 #endif // CWT_H
